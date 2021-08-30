87ec0a84ba3987085f2187571fad351fb7b03520
rmedx <65210483+rmedx@users.noreply.github.com>
2021-08-30 17:47:32 -0400

    finished

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -80,32 +80,29 @@ suite('Functional Tests with Zombie.js', function () {
   });
 
   suite('"Famous Italian Explorers" form', function () {
-    #5
-    test('Submit the surname "Colombo" in the HTML form', (done) => {
-      browser.fill('surname', 'Colombo').then(() => {
+    // #5
+    test('Submit the surname "Colombo" in the HTML form', function (done) {
+      browser.fill('surname', 'Colombo', async () => {
         browser.pressButton('submit', () => {
           browser.assert.success();
           browser.assert.text('span#name', 'Cristoforo');
           browser.assert.text('span#surname', 'Colombo');
           browser.assert.element('span#dates', 1);
-          done();
+          done(); 
         });
-      });
-    });
-    test('submit "surname" : "Colombo" - write your e2e test...', function(done) {
-      browser.fill('surname', 'Colombo').pressButton('submit', function() {
-        browser.assert.success();
-        browser.assert.text('span#name', 'Cristoforo');
-        browser.assert.text('span#surname', 'Colombo');
-        browser.assert.element('span#dates', 1);
-        done(); 
       });
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill('surname', 'Vespucci', async () => {
+        browser.pressButton('submit', () => {
+          browser.assert.success();
+          browser.assert.text('span#name', 'Amerigo');
+          browser.assert.text('span#surname', 'Vespucci');
+          browser.assert.element('span#dates', 1);
+          done(); 
+        });
+      });
     });
   });
 });
